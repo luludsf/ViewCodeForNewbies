86b0bfb4438d056ad878ebd4f297b13478f7c72d
Luana Duarte <luanaduarte@Luanas-MacBook-Air.local>
2022-10-06 21:13:25 -0300

    remoção de comentários

--- ViewCodeForNewbies/ViewController.swift
+++ ViewCodeForNewbies/ViewController.swift
@@ -12,9 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
