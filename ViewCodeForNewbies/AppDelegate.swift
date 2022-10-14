25d8f68213c71d26f9eb017c346a9d6dfb3ead11
Alfin <m.alfin.syahruddin@gmail.com>
2022-10-14 20:31:10 +0700

    Change root VC

--- ViewCodeForNewbies/AppDelegate.swift
+++ ViewCodeForNewbies/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = CollectionViewController()
+        window?.rootViewController = ViewController()
 
         return true
     }
