7cb5947199af98143928a1c0959bd2d579cbd366
Ricardo Sena <kakaxsena@gmail.com>
2022-10-18 19:37:58 -0300

    Created UIView extension
    closes #8

---
+++ ViewCodeForNewbies/UIViewExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewExtensions.swift
+//  ViewCodeForNewbies
+//
+//  Created by Luana Duarte on 07/10/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public func setConstraintsToSuperview(top: CGFloat = 0, leading: CGFloat = 0,
+                                          trailing: CGFloat = 0, bottom: CGFloat = 0) {
+        guard let superview else { return }
+        let safeArea = superview.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: safeArea.topAnchor, constant: top),
+            leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: leading),
+            trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: trailing),
+            bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: bottom)
+        ])
+    }
+}
